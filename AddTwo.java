56a7b8ae45c53125a42989fe7eb7b9e7fc29744a
ErezYousfan <154258934+ErezYousfan@users.noreply.github.com>
2023-12-18 21:18:41 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -4,5 +4,9 @@
 public class AddTwo {
 	public static void main(String[] args) {
 		// Put your code here	
+		int	var1 = Integer.parseInt(args[0]);
+		int var2 = Integer.parseInt(args[1]);
+		int sum = var1 + var2;
+		System.out.println(var1 + " + " + var2 + " = " + sum);
 	}
 }
