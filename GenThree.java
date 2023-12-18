ee93ff8e600559e86e40f57e42adb8cb9b1abd05
ErezYousfan <154258934+ErezYousfan@users.noreply.github.com>
2023-12-18 22:20:05 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -5,5 +5,22 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Put your code here	
+		int min = Integer.parseInt(args[0]);
+		int max = Integer.parseInt(args[1]);
+		int diff = max - min;
+		int first = ThreadLocalRandom.current().nextInt(min, max);
+		int second = ThreadLocalRandom.current().nextInt(min, max);
+		int third = ThreadLocalRandom.current().nextInt(min, max);
+		int minnum = first;
+		if( minnum > second){
+			minnum = second;
+		}
+		if(minnum > third){
+			minnum = third;
+		}
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+		System.out.println("The minimal generated number was " + minnum);
 	}
 }
